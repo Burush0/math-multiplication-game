47242fcaf103e081c345831059535161547d875a
Burush0 <burush02@gmail.com>
2025-03-08 14:21:48 +0100

    Added basic back and forth data flow between client and server

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 # app instance
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/api/submit', methods=['POST'])
+def handle_submission():
+    data = request.json
+    print("Received data:", data)
+
+    response = {"status": "success", 
+                "message": "Data received"}
+    return jsonify(response)
 
 @app.route("/api/home", methods=['GET'])
 def return_home():
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -6,6 +6,26 @@ function index() {
 
   const [message, setMessage] = useState("Loading");
   const [people, setPeople] = useState([]);
+  const [response, setResponse] = useState(null);
+
+  const handleClick = async () => {
+    const data = { message: "Hello from React!" };
+
+    try {
+      const response = await fetch("http://localhost:8080/api/submit", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      const result = await response.json();
+      setResponse(result);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
   useEffect(() => {
     fetch("http://localhost:8080/api/home")
@@ -18,6 +38,12 @@ function index() {
 
   return (
     <div>
+      <button onClick={handleClick}>
+        Send Data to Server
+      </button>
+      {response && <p>
+        Server Response: {JSON.stringify(response)}
+        </p>}
       <div>{message}</div>
 
       {people.map((person, index) => (
