a4903418fd3ae9a5530cb864cc74beb7bd1e9993
Burush0 <burush02@gmail.com>
2025-03-09 19:51:38 +0100

    Refacted frontend completely, integrated AuthJS with GitHub as a test

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,28 @@
+"use server"
+
+import { auth } from "@/auth";
+import { SignInButton } from "./components/sign-in-button";
+import Image from "next/image";
+import Link from "next/link";
+import { SignOutButton } from "./components/sign-out-button";
+
+export default async function Home() {
+  const session = await auth();
+
+  if (session?.user) {
+    return (
+      <div>
+        <Link href="/user-info"> User Info </Link>
+        <SignOutButton />
+      </div>
+    )
+  }
+
+  return (
+  <div>
+    {" "} 
+    <p> You are not signed in </p>{" "}
+    <SignInButton />
+  </div>
+  )
+}
---
+++ frontend/src/app/user-info/page.tsx
@@ -0,0 +1,22 @@
+import { auth } from "@/auth";
+import Image from "next/image";
+
+export default async function UserInfo() {
+    const session = await auth();
+    return (
+        <div>
+            {" "}
+            <p> User signed in with name: {session?.user?.name}</p>
+            <p> User signed in with email: {session?.user?.email}</p>
+            {session?.user?.image && (
+                <Image 
+                src={session.user.image} 
+                width={48} 
+                height={48} 
+                alt={session.user.name ?? "avatar"}
+                style={{borderRadius: "50%"}}
+                />
+            )}
+        </div>
+      )
+    }
